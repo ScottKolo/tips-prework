a8c649e7605a2c180cdda40ae593b8a396814c4f
Scott Kolodziej <spkolodziej@gmail.com>
2015-12-28 23:02:52 -0600

    Add lots of features - good/neutral/bad tips, revised navigation controller, add background and Font Awesome.

--- tips/SettingsViewController.swift
+++ tips/SettingsViewController.swift
@@ -9,8 +9,14 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
-    @IBOutlet weak var defaultTipControl: UISegmentedControl!
+    
+    @IBOutlet weak var goodTipLabel: UILabel!
+    @IBOutlet weak var neutralTipLabel: UILabel!
+    @IBOutlet weak var badTipLabel: UILabel!
+    
+    @IBOutlet weak var goodTipSlider: UISlider!
+    @IBOutlet weak var neutralTipSlider: UISlider!
+    @IBOutlet weak var badTipSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,21 +33,85 @@ class SettingsViewController: UIViewController {
         super.viewWillAppear(animated)
         
         let defaults = NSUserDefaults.standardUserDefaults()
-        let defaultTipIndex = defaults.integerForKey("defaultTip")
+        var defaultGoodTip:Int
+        var defaultNeutralTip:Int
+        var defaultBadTip:Int
         
-        defaultTipControl.selectedSegmentIndex = defaultTipIndex
+        // Check if tips are stored in user defaults - if not, use 10/20/25%
+        if defaults.objectForKey("defaultGoodTip") != nil {
+            defaultGoodTip    = defaults.integerForKey("defaultGoodTip")
+        }
+        else {
+            defaultGoodTip = 25
+            defaults.setInteger(25, forKey: "defaultGoodTip")
+        }
+        
+        if defaults.objectForKey("defaultNeutralTip") != nil {
+            defaultNeutralTip    = defaults.integerForKey("defaultNeutralTip")
+        }
+        else {
+            defaultNeutralTip = 20
+            defaults.setInteger(20, forKey: "defaultNeutralTip")
+        }
+        
+        if defaults.objectForKey("defaultBadTip") != nil {
+            defaultBadTip    = defaults.integerForKey("defaultBadTip")
+        }
+        else {
+            defaultBadTip = 10
+            defaults.setInteger(10, forKey: "defaultBadTip")
+        }
+        
+        goodTipSlider.value    = Float(defaultGoodTip)
+        neutralTipSlider.value = Float(defaultNeutralTip)
+        badTipSlider.value     = Float(defaultBadTip)
+        
+        goodTipSliderChanged(self)
+        neutralTipSliderChanged(self)
+        badTipSliderChanged(self)
     }
     
-    @IBAction func defaultTipChanged(sender: AnyObject) {
+    @IBAction func goodTipSliderChanged(sender: AnyObject) {
+        // Good tip slider has changed
+        // Make sure good tip is greater than other tips
+        if goodTipSlider.value <= neutralTipSlider.value {
+            goodTipSlider.value = neutralTipSlider.value
+        }
+        goodTipLabel.text = String(format: "%d%%", Int(round(goodTipSlider.value)))
+        
+        let newDefaultTip = Int(round(goodTipSlider.value))
         let defaults = NSUserDefaults.standardUserDefaults()
-        defaults.setInteger(defaultTipControl.selectedSegmentIndex, forKey: "defaultTip")
-        defaults.synchronize()
+        defaults.setInteger(newDefaultTip, forKey: "defaultGoodTip")
     }
     
-    @IBAction func dismissSettings(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: {})
+    @IBAction func neutralTipSliderChanged(sender: AnyObject) {
+        // Neutral tip slider has changed
+        // Make sure neutral tip is between good and bad tips
+        if neutralTipSlider.value >= goodTipSlider.value {
+            neutralTipSlider.value = goodTipSlider.value
+        } else if neutralTipSlider.value <= badTipSlider.value {
+            neutralTipSlider.value = badTipSlider.value
+        }
+        neutralTipLabel.text = String(format: "%d%%", Int(round(neutralTipSlider.value)))
+        
+        let newDefaultTip = Int(round(neutralTipSlider.value))
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(newDefaultTip, forKey: "defaultNeutralTip")
     }
-
+    
+    @IBAction func badTipSliderChanged(sender: AnyObject) {
+        // Bad tip slider has changed
+        // Make sure bad tip is less than other tips
+        if badTipSlider.value >= neutralTipSlider.value {
+            badTipSlider.value = neutralTipSlider.value
+        }
+        badTipLabel.text = String(format: "%d%%", Int(round(badTipSlider.value)))
+        
+        let newDefaultTip = Int(round(badTipSlider.value))
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(newDefaultTip, forKey: "defaultBadTip")
+    }
+    
     /*
     // MARK: - Navigation
 
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -14,13 +14,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    
+    // Stored good/neutral/bad tip percentages
+    var goodTip: Int = 25
+    var neutralTip: Int = 20
+    var badTip: Int = 10
+    
+    func getTipPercentage() -> Int {
+        // Get tip percentage from slider position and good/neutral/bad tip values
+        let sliderPosition = tipSlider.value
+        if sliderPosition < 0.5 {
+            return Int(round(Float(badTip) + Float(neutralTip - badTip)*sliderPosition*2))
+        }
+        else {
+            return Int(round(Float(neutralTip) + Float(goodTip - neutralTip)*(sliderPosition-0.5)*2))
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let image = UIImage(named: "fabric_of_squares_gray_@2X.png") {
+            view.backgroundColor = UIColor(patternImage: image)
+        } else {
+            print("There was no such image as small_steps.png")
+        }
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
+        tipSlider.value = 0.5
+        onTipSliderChange(self) // Update slider UI
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,16 +53,14 @@ class ViewController: UIViewController {
 
     @IBAction func onEditingChanged(sender: AnyObject) {
         
-        var tipPercentages = [0.18, 0.2, 0.22]
-        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let tipPercentage = Float(getTipPercentage())/100
         
-        var billAmount = NSString(string: billField.text!).doubleValue
-        var tip = billAmount * tipPercentage
-        var total = billAmount + tip
+        let billAmount = NSString(string: billField.text!).floatValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
-        
     }
 
     @IBAction func onTap(sender: AnyObject) {
@@ -51,11 +72,19 @@ class ViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
+        // Get user defaults for good/neutral/bad tip values
         let defaults = NSUserDefaults.standardUserDefaults()
-        let defaultTipIndex = defaults.integerForKey("defaultTip")
+        goodTip = defaults.integerForKey("defaultGoodTip")
+        neutralTip = defaults.integerForKey("defaultNeutralTip")
+        badTip = defaults.integerForKey("defaultBadTip")
         
-        tipControl.selectedSegmentIndex = defaultTipIndex
+        tipSlider.value = 0.5   // Reset tip back to neutral
+        onTipSliderChange(self) // Update slider UI
     }
     
+    @IBAction func onTipSliderChange(sender: AnyObject) {
+        tipPercentLabel.text = String(format: "%d%%", getTipPercentage())
+        onEditingChanged(self)
+    }
 }
 
